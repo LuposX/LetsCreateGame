9e009cb75cc0aefb59c4eb35d4421229553d616b
LuposX <schuppsimon5@gmail.com>
2020-04-03 19:43:20 +0200

    removed static. added Controls

---
+++ CreateGame/src/logic/Controls.java
@@ -0,0 +1,27 @@
+package logic;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Controls {
+	/* Die methoden "render" und "update" werden in allen level aufgerufen.
+	 * Hier kommt alles rein was man in beiden leveln haben will.
+	 * 
+	 */
+	
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+	}
+	
+	
+	public void render(GameContainer gc, Graphics g) {
+		
+	}
+	
+	public void update(GameContainer gc, StateBasedGame sbg, int t) {
+		
+	}
+}
