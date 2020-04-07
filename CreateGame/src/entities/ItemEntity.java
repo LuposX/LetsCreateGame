d1e165a14063e0c2c96fc06e4052edeb94609264
GloeStoe <karl-bernhard1@gmx.de>
2020-04-07 13:43:18 +0200

    Beispiele und Kommentare zu #25

--- CreateGame/src/entities/ItemEntity.java
+++ CreateGame/src/entities/ItemEntity.java
@@ -24,10 +24,13 @@ public class ItemEntity extends Entity{
 
 	@Override
 	public void render(GameContainer gc, Graphics g) {
-		//Hier soll das Item gezeichnet werden.
+		//Hier soll das Item gezeichnet werden. Um zu ueberpruefen um welches
+		//Item es sich handelt nutze "theItem instanceof ItemTest"
 		shape = new Rectangle(drawX-5, drawY-5, 10, 10);
 		g.setColor(Color.gray);
 		g.fill(shape);
+		g.setColor(Color.orange);
+		g.drawString(theItem.displayName, drawX+5, drawY); //Statt Text, Bild einfuegen
 	}
 
 }
