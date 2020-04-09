ac540e0ad8c39bc8fc234e0d6f44fb477693189e
GloeStoe <karl-bernhard1@gmx.de>
2020-04-10 00:20:30 +0200

    Fixing Agro

--- CreateGame/src/entities/hostile/Slime.java
+++ CreateGame/src/entities/hostile/Slime.java
@@ -25,9 +25,9 @@ public class Slime extends Hostile{
 		if(PathFinder.isEyeContact(this, target)){
 			runTo(target.posX, target.posY);
 			agro += dt;
-			agro = Math.max(agro, 3000);
+			agro = Math.min(agro, 3000);
 		} else if(agro > 0) {
-			agro -= dt/4;
+			agro -= dt/8;
 			ArrayList<int[]> path = PathFinder.findPath((int) posX, (int) posY, (int) target.posX, (int) target.posY);
 			if(path.size() > 1) {
 				runTo(path.get(path.size()-2)[0]+0.5f, path.get(path.size()-2)[1]+0.5f);
